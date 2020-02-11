a2811894c75137190b8917bff00e39da943b165e
alexshavlovsky <ctzn@tut.by>
2020-02-11 14:37:19 +0300

    Initial commit

---
+++ src/main/java/com/ctzn/webfluxtelegrambotclient/WebFluxTelegramBotClientApplication.java
@@ -0,0 +1,13 @@
+package com.ctzn.webfluxtelegrambotclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebFluxTelegramBotClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebFluxTelegramBotClientApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ctzn/webfluxtelegrambotclient/WebFluxTelegramBotClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ctzn.webfluxtelegrambotclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebFluxTelegramBotClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
