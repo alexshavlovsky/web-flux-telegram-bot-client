27e62aba6c071b40763695f6d5fd850710cd2d5e
alexshavlovsky <ctzn@tut.by>
2020-02-12 20:48:56 +0300

    refactor: DOM based chat messages

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -1,12 +1,17 @@
-const elConsole = document.getElementById('console');
 const elInput = document.getElementById("msg-input");
+const elConsole = document.getElementById("console");
+const elConsoleContainer = document.getElementById("console-container");
 
-elConsole.value = "";
 elInput.addEventListener("keyup", sendOnEnter);
 
 function print(s) {
-    elConsole.value += s + "\n";
-    elConsole.scrollTop = elConsole.scrollHeight;
+    let msg = document.createElement('div');
+    msg.classList.add('alert');
+    msg.classList.add('alert-primary');
+    msg.setAttribute("role", "alert");
+    msg.innerHTML = s;
+    elConsole.appendChild(msg);
+    elConsoleContainer.scrollTop = elConsoleContainer.scrollHeight;
 }
 
 function sendOnEnter(event) {
@@ -19,12 +24,12 @@ function sendOnEnter(event) {
 let wsClient = new WebSocket("ws://localhost:8080/messages/" + token);
 wsClient.onopen = function () {
 };
-wsClient.onclose = function (error) {
+wsClient.onclose = function () {
     print("Closing connection");
 };
-wsClient.onerror = function (error) {
+wsClient.onerror = function () {
     print("An error occurred");
 };
-wsClient.onmessage = function (error) {
-    print(error.data);
+wsClient.onmessage = function (event) {
+    print(event.data);
 };
